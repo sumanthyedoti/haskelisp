15cef667965297bb419152a8f94fa821b60d4124
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-06-15 11:53:47 +0530

    Implement REPL

--- app/Main.hs
+++ app/Main.hs
@@ -6,9 +6,22 @@ import Control.Monad
 import Env (eval)
 import Errors
 import Parser
+import System.IO
+
+readExpr :: IO String
+readExpr = putStr "hlisp> " >> hFlush stdout >> getLine
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = do
+  evaled <- return $ liftM show (parse expr >>= eval)
+  putStrLn $ extractValue $ trapError evaled
+
+runRepl_ :: IO ()
+runRepl_ = do
+  input <- readExpr
+  if (input == ":quit" || input == ":q")
+    then return ()
+    else evalAndPrint input >> runRepl_
 
 main :: IO ()
-main = do
-  (expr:_) <- getArgs
-  evaled <- return $ liftM show (parse (expr) >>= eval)
-  putStrLn $ extractValue $ trapError evaled
+main = runRepl_
